01d6c1f9449122154ba8759abcef65145c6033ce
Pallavi Deore <pallavi.deore@VEROLT.COM>
2022-09-29 12:20:06 +0530

    two number additions

--- add.cpp
+++ add.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number, third_number, sum;
+  int first_number, second_number, sum;
     
   cout << "Enter two integers: ";
-  cin >> first_number >> second_number >> third_number;
+  cin >> first_number >> second_number ;
 
   // sum of two numbers in stored in variable sumOfTwoNumbers
-  sum = first_number + second_number + third_number;
+  sum = first_number + second_number ;
 
   // prints sum 
-  cout << first_number << " + " <<  second_number << " + " <<  third_number << " = " << sum;  
+  cout << first_number << " + " <<  second_number << " + " <<  " = " << sum; 
